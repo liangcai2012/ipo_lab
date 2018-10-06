d3a1dd58d66f9a8eb07d834586a52b40cab54e17
Liang Cai <liangcai@Liangs-Mac-mini.local>
2018-10-06 15:37:39 -0700

    process.py modified

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0, '..')
 import ipo_lab
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def pop_by_hot(df, h_scope, p_scope):
     hot = []
@@ -40,13 +40,22 @@ def distribution_pm(df, valve):
             pm_over_valve[year]+=1
     return range(start_year, end_year+1), total, pm_over_valve
 
-data = ipo_lab.load_data("./ipo_open.csv")
-h, p = pop_by_hot(data, 2, 3)
-#years, total, pov = distribution_pm(data, 1.02)
-#for i in range(len(years)):
-#    print years[i], "%.2f"%(pov[i]*1.0/total[i])
-#p1 = plt.bar(years, total, 0.35)
-#p2 = plt.bar(years, pm_ratio, 0.35)
-#plt.show()
+def parse_uw():
+    import pandas as pd
+    df = pd.read_csv('./ipo_uws.csv')
+    uw_list = df["underwriter"]
+    
+    print uw_list
+    uw_set = set(uw_list)
+    print len(uw_list)
+    print len(uw_set)
+
+
+
+#data = ipo_lab.load_data("./ipo_open.csv")
+#h, p = pop_by_hot(data, 2, 3)
+
+parse_uw()
+
 
 
