8d40d66dd0ea0aa98356f2b92ce8ff1ea2f22ba0
Liang Cai <liangcai@Liangs-MacBook-Pro.local>
2018-11-05 20:53:15 -0800

    changes

--- __init__.py
+++ __init__.py
@@ -1,24 +1,52 @@
 import pandas as pd
+from datetime import datetime
 
-def load_data(path):
+_p = lambda x:"%.2f"%x
+_dt = lambda x: datetime.strptime(str(x), "%H%M%S")
+
+def load_data_sort_by_date(path):
     df = pd.read_csv(path, index_col=[0], date_parser=lambda x:pd.datetime.strptime(x, "%Y%m%d"))
     sdf = df.sort_index()
     return sdf
 
-
-def histo(path, col, n, valve, dt):
+def load_data(path):
     df = pd.read_csv(path)
+    return df
+
+def distribution(df, col, num):
+    sdf = df.sort_values(by=[col])
+    lb = sdf.iloc[0][col]
+    rb = sdf.iloc[-1][col]
+    step = (rb - lb)/num
+    x = []
+    y = []
+    start = lb
+    n = 0
+    for i, row in sdf.iterrows():
+        if row[col] > start + step:
+            x.append(start)
+            y.append(n)
+            n = 0
+            start += step
+            for i in range(int((row[col] - start)/step)):
+                x.append(start)
+                y.append(0)
+                start += step
+        n += 1
+    return x, y, step
+
+def histo(df, col, n, valve, dt):
     num = len(df)/n 
     if len(df)%n > 0:
         num += 1
-    sdf = df.sort_values(by=['symbol'])
+    sdf = df.sort_values(by=[col])
     idx = -1
     t = 0
     f = 0
     sec = 1 
     xax = []
     yax = []
-    for i, row in df.iterrows():
+    for i, row in sdf.iterrows():
         idx += 1
         if idx > num * sec:
             xax.append(row[col])
@@ -35,3 +63,165 @@ def histo(path, col, n, valve, dt):
             f += 1
 
     return xax, yax
+
+def luw_dist(df, valve, top):
+    res = [[0,0]]
+    for i, row in df.iterrows():
+        struws = row["underwriter"] 
+        try:
+            uws = row["underwriter"].split('-')
+        except:
+            continue
+        for uw in uws:
+            uwint = int(uw)
+            if uwint > len(res):
+                for k in range(uwint - len(res)):
+                    res.append([0,0])
+            res[uwint-1][0] += 1
+            if row['y'] > valve:
+                res[uwint - 1][1] += 1
+    res_df  = pd.DataFrame(data = res, columns=['num', 'p1'])
+    sdf = res_df.sort_values(by="num", ascending=False).iloc[:top] 
+    id = []
+    num = []
+    p = []
+
+    for i, row in sdf.iterrows():
+        id.append(i)
+        num.append(row["num"])
+        p.append(row["p1"]*1.0/row["num"])
+
+    return id, num, p
+
+
+def exchange_dist(df, valve):
+    res = [[0,0], [0,0],[0,0]]
+    for i, row in df.iterrows():
+        if row["exchange"] < 1 or row["exchange"]>3:
+            print 'unexpected exchange', row[symbol], row['exchange']
+            continue
+        res[row["exchange"]-1][0]+= 1
+        if row['y']>valve:
+            res[row["exchange"]-1][1]+=1
+    return res
+  
+def simple_filter(df, valve):
+    sum = 0
+    p = 0
+    p30 = p1h = p1d = 0
+    bin = [0]*12
+    profit = 0
+    for i, row in df.iterrows():
+#        if 0.955 < row["x1"]<1.035: 
+#            continue
+#        if row["x2"] > 180000:
+#            continue
+#        if row["t10"]<93200:
+#            continue
+#        uwlist = row["underwriter"].split('-')
+#        found = False
+#        for u in uwlist:
+#            if u == '13':
+#                found = True
+#                break
+#        if found:
+#            continue
+
+#      the following filtered out 22 symbols and 11 are good
+#        if  0.6 < row["x3"]< 0.9:
+#            continue
+        
+##      The following filtered out 138 symbols and 95 are good         
+#        if row['exchange'] != 1:
+#            print row["symbol"], row["y"]
+#           continue
+##       The following filter only filtered out 3 symbols while 2 of them are good
+#        if len(row["symbol"]) > 4:
+#            print row["symbol"], row["y"]
+#            continue
+##       The following filter only filtered out 4 symbols while 2 of them are good        
+#        if 0.75 < row["x3"]<0.9:
+#            print row["symbol"], row["y"]
+#            continue
+
+        sum += 1
+#        if row["p1d"] > valve:
+        if row["p1d"] > 1.01:
+            p += 1
+#            profit += 0.02
+            profit += row["pc1d"] - 1.01
+        else:
+#            if row["y"] == 1 and row["p1d"] == 1:
+#                print row["symbol"], row["x1"], row["x3"]
+            if row["p30"] > valve:
+                p30+=1
+            if row["p1h"] > valve:
+                p1h += 1
+            if row["p1d"] > valve:
+                p1d += 1
+#            if row["pc10"] < 0.9:
+#                print row["symbol"], row["y"], row["t10"], row["x1"], row["x3"], row["pl10"], row["tl10"], row["pc10"]
+#            profit += row["pc1d"] - 1
+
+#        if 0 <= row["t10"]<=11:
+#            bin[row["t10"]] += 1
+#        if 10<= row["t10"] <= 11:
+#            if row["p30"] < row["y"]:
+#                print row["symbol"], row["y"], row["t10"], row["p30"], row["t30"]
+    print p, p30, p1h, p1d, sum, p*1.0/sum, sum*1.0/len(df)
+#    print bin 
+    print 'profit:', profit
+
+# but when price exceed in a time frame
+def simple_strat_2(df, col, valve, vhigh, years):
+    total_profit = 0
+    print vhigh, '---', 
+    for y in years:
+        
+        total = 0
+        triggered = 0
+        bad = 0
+        missing = 0
+        profit = 0
+        bad_profit = 0
+        missing_profit = 0
+        above_valve = 0
+        for i, row in df.iterrows():
+            d = row["date"]
+            if y != d/10000:
+                continue
+#            if 0.975 < row["x1"]<1.018: 
+#                continue
+#            if row["x2"] > 180000:
+#                continue
+            total += 1
+            if row["p1m"]>=valve or row["y"] >= vhigh:
+                triggered += 1
+
+                if row["y"] >=vhigh:
+                    profit += vhigh-1.0 
+                    above_valve += 1
+                else:
+                    profit += row[col] - valve
+
+                if row["y"] < vhigh and row[col] < valve:
+                    bad += 1
+                    bad_profit += row[col]-valve
+                else:
+                    topen = _dt(row["topen"])
+                    t1m= _dt(row["t1m"])
+                    t10 = _dt(row["t10"])
+#                    print row["symbol"], row["y"], row["p1m"],  row["pc10"], (t1m-topen).total_seconds(), (t10-topen).total_seconds() 
+#                if 1.04 <= row["y"] < 1.05:
+#                    print row["symbol"], row["pc10"]
+            else:
+                profit += row["pc10"] - 1
+                if row[col] > valve:
+                    missing += 1
+                    missing_profit += row[col]-valve
+        if total == 0:
+            continue
+        total_profit += profit
+#    print y, total, _p(triggered*1.0/total), _p(above_valve*1.0/total),  _p(bad*1.0/total), _p(missing*1.0/total), profit, bad_profit, missing_profit
+        print profit, 
+    print total_profit
--- process.py
+++ process.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '..')
 import ipo_lab
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def detect_overlap():
     df= ipo_lab.load_data("./ipo_open.csv")
@@ -17,6 +17,58 @@ def detect_overlap():
             prev_sym = row["symbol"]
 
     
+def merge():
+    df_open = ipo_lab.load_data("./ipo_open.csv")
+    sdf_open = df_open.sort_values(by=['symbol'])
+    with open("./ipo5.csv") as f:
+        line = f.readline().strip()
+        ls = line.split(',')
+        ls.insert(1, "date")
+        print ','.join(ls)
+        line = f.readline().strip()
+        while line:
+            ls = line.split(',')
+            symbol = ls[0]
+            d= df_open.loc[df_open["symbol"] == symbol].iloc[0, 0]
+            if d is None:
+                print "--------- missing", symbol
+                continue
+            ls.insert(1,str(d))
+            print ','.join(ls)
+            line = f.readline().strip()
+        print line
+
+    exit()
+    df= ipo_lab.load_data("./ipo.csv")
+    sdf = df.sort_values(by=["symbol"])
+
+    print "symbol,date,y,x1,x2,x3,x4,x5,underwriter,exchange"
+    j = 0
+    for i in range(len(sdf_open)):
+        row_open = sdf_open.iloc[i]
+        row = sdf.iloc[j]
+        while row["symbol"] < row_open["symbol"]:
+#            print "missing in ipo_open.csv", row["symbol"], row_open["symbol"]
+            j+= 1
+            row = sdf.iloc[j]
+
+        if row["symbol"] > row_open["symbol"]:
+#            print "missing in ipo.csv", row["symbol"], row_open["symbol"]
+            continue
+
+        line = []
+        line.append(row["symbol"])
+        line.append(str(row_open["date"]))
+        line.append("%.3f"%row["y"])
+        line.append("%.3f"%row["x1"])
+        line.append(str(row["x2"]))
+        line.append("%.3f"%row["x3"])
+        line.append("%.3f"%row["x4"])
+        line.append(str(row_open["minutes"]))
+        line.append(str(row["underwriter"]))
+        line.append(str(row["exchange"]))
+        print ','.join(line)
+        j+=1
 
 
 def pop_by_hot(df, h_scope, p_scope):
@@ -104,8 +156,8 @@ def svm(training_size, valve):
             y_train.append(1)
         else:
             y_train.append(0)
-#        x_train.append([row["x1"], row["x2"], row["x3"], row["x4"], row["exchange"]])
-        x_train.append([row["x1"]])
+        x_train.append([row["x1"], row["x2"], row["x3"], row["x4"], row["exchange"]])
+#        x_train.append([float(row["x1"]), float(row["x2"])])
 #    print y_train
 #    print x_train
     clf = svm.SVC(gamma='scale',kernel='rbf')
@@ -119,8 +171,8 @@ def svm(training_size, valve):
         no += 1
         if no <= training_size:
             continue
-        #if(1 == clf.predict([[row["x1"], row["x2"], row["x3"], row["x4"], row["exchange"]]])[0]):
-        p = clf.predict([[row["x1"]]])
+#        if(1 == clf.predict([[row["x1"], row["x2"], row["x3"], row["x4"], row["exchange"]]])[0]):
+        p = clf.predict([[row["x1"], row["x2"], row["x3"], row["x4"], row["exchange"]]])
 #        print p
         if 1 == p[0]:
             if row["y"] > valve:
@@ -146,8 +198,115 @@ def svm(training_size, valve):
 #svm(700, v)
 #filter_by_exch(v)
 
+#plot distribution y, x1-x4
+def plot_y_dist():
+    df = ipo_lab.load_data("./ipo5.csv")
+    x, y, step = ipo_lab.distribution(df, 'p1m', 100)
+    print len(x)
+    plt.bar(x[:40], y[:40], width=step)
+    plt.show()
+
+def print_min_dist():
+    dfm= ipo_lab.load_data("./ipo3.csv")
+    bin = [0]*11
+    for i, row in dfm.iterrows():
+        if 0 <= row["t10"]<=11:
+            bin[row["t10"]-1] += 1
+        if 10<= row["t10"] <= 11:
+            if row["p30"] < row["y"]:
+                print row["symbol"], row["y"], row["t10"], row["p30"], row["t30"]
+        else:
+            #print "unexpected minutes", row["symbol"], row["t10"]
+            continue
+#    print bin
+    
+    
+#plot distribution x1-x4
+def plot_x1_4_dist():
+    df = ipo_lab.load_data("./ipo.csv")
+    fig, axs = plt.subplots(2, 2, sharey=True, tight_layout=True)
+    cols = ['x1','x2','x3','x4']
+    ends = [50,80,30,50]
+#    ylims = [1200, 1200, 300, 300]
+    for i in range(4):
+        x, y, step = ipo_lab.distribution(df, cols[i], 100)
+        axs[i/2][i%2].bar(x[:100-ends[i]], y[:100-ends[i]], width=step)
+        axs[i/2][i%2].set_title('distribution of '+cols[i])
+#        axs[i/2][i%2].set_ylim(0, ylims[i])
+        
+    plt.show()
+
+#plot step x1-x4 with y wehen valve = 1.02
+def plot_y_vs_x1_4_v102():
+    df = ipo_lab.load_data("./out_bad.csv")
+    fig, axs = plt.subplots(2, 2, sharey=True, tight_layout=True)
+    cols = ['x1','x2','x3','x4']
+    for i in range(4):
+        x, y = ipo_lab.histo(df, cols[i], 20, 1.01, 1)
+        print 'distribution of', cols[i]
+        for l in range(len(x)):
+            print x[l], y[l]
+        axs[i/2][i%2].step(x, y)
+        axs[i/2][i%2].set_title('y/'+cols[i])
+    plt.show()
+
+#plot step x1 with y when valve changes    
+def plot_y_vs_x1_v101_104():
+    df = ipo_lab.load_data("./ipo.csv")
+    fig, axs = plt.subplots(2, 2, sharey=True, tight_layout=True)
+    for i in range(101, 105):
+        v = i/100.0
+        x, y = ipo_lab.histo(df, "x1", 20, v, 1)
+        l = "vavle = %.2f"%v
+        m = i - 101
+        axs[m/2][m%2].step(x, y)
+        axs[m/2][m%2].set_title(l)
+    plt.show()
+
+def plot_y_vs_luw():
+    df = ipo_lab.load_data("./ipo.csv")
+    id, num, p = ipo_lab.luw_dist(df, 1.02, 25)
+    print id, num, p
+    x = []
+    sum = 0
+    for n in num:
+        x.append(sum + n)
+        sum += n
+    plt.step(x, p)
+    plt.show()
+
+def print_y_vs_exchange():
+    df = ipo_lab.load_data("./ipo.csv")
+    ex_res = ipo_lab.exchange_dist(df, 1.02)
+    sum = 0
+    p = 0
+    exchange = ['NASDAQ','NYSE','AMEX']
+    for i in range(len(ex_res)):
+        r = ex_res[i]
+        print exchange[i], r[1]*1.0/r[0], r[0]
+        sum+= r[0]
+        p += r[1]
+    print "TOTAL", p*1.0/sum, sum
+    
+#def dist_t10():
+#    df = ipo_lab.load_data("./ipo5.csv")
+#    for i, row in df.iterrows():
 
 
-x, y = ipo_lab.histo('./ipo.csv', "x1", 15, 1.02, 1)
-print x
-print y
+#merge()
+#exit()
+#plot_y_dist()
+#plot_x1_4_dist()
+#plot_y_vs_x1_4_v102()
+#plot_y_vs_x1_v101_104()
+#plot_y_vs_luw()
+#print_y_vs_exchange()
+#exit()
+
+#detect_overlap()
+#print_min_dist()
+df = ipo_lab.load_data("./ipo5.csv")
+#ipo_lab.simple_filter(df, 1.02)
+for i in range(102, 115):
+    ipo_lab.simple_strat_2(df, "pc10", 1.0001, i/100.0, range(2010, 2019))
+#svm(1000, 1.02)
